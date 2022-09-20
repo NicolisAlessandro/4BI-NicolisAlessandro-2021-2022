4fb29f6f6e588b11412e6549b7639710190b98d9
NicolisAlessandro <19131@studenti.marconiverona.edu.it>
2022-09-20 13:39:40 +0200

    udp_01

---
+++ src/nicolis_A_Udp_01/bin/Udp_01.java
@@ -0,0 +1,31 @@
+package nicolis_A_Udp_01.bin;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.SocketException;
+
+class Udp_01Test {
+	public static void main(String[] args) throws IOException {
+
+		InetAddress ip1 =InetAddress.getByName("172.16.1.99");
+		int port;
+		DatagramSocket socket = new DatagramSocket();
+		DatagramPacket p_send,p_rece;
+
+		port=13;// 7 echo
+		// 13 dayTime
+
+		String mex="meprova";
+		byte[] sendBuf = mex.getBytes();
+		p_send = new DatagramPacket(sendBuf, sendBuf.length,ip1,port);
+		byte[] recBuf = new byte[256];
+		p_rece=new DatagramPacket(recBuf,recBuf.length);
+		socket.send(p_send);
+		socket.receive(p_rece);
+		String rec=new String(recBuf,0,p_rece.getLength());
+
+
+	}
+}
